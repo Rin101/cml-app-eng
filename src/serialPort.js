6d63bd764179f9c59133ec3c1fd179d3a0d5acd9
Rin101 <wayoftheforce101@gmail.com>
2023-01-31 17:52:03 +0900

    loop design

--- src/serialPort.js
+++ src/serialPort.js
@@ -37,7 +37,7 @@ export async function pressRun (cml) {
             const writer = await port.writable.getWriter();
             await writer.write(str2ab(cmlChunk + '\r\n'));
             await writer.releaseLock();
-            console.log(cmlChunk + '\r\n')
+            // console.log(cmlChunk + '\r\n')
         }
     }
     // const writer = await port.writable.getWriter();
@@ -47,37 +47,13 @@ export async function pressRun (cml) {
 }
 
 export async function send () {
-    // const port = await navigator.serial.requestPort();
-    // await port.open({ baudRate: 38400 });
-    // const writer = port.writable.getWriter();
-    // await writer.write(str2ab('S.1=100\r\n'));
-    // await writer.write(str2ab('A.1=100\r\n'));
-    // await writer.write(str2ab('P.1=100000\r\n'));
-    // await writer.write(str2ab('^.1\r\n'));
-    // // ==============
-    // // await writer.write(str2ab('P.1=0\r\n'));
-    // // await writer.write(str2ab('^.1\r\n'));
-    // writer.releaseLock();
-    // await port.close();
-
-
-    // cml = "S1.1=100\nA1.1=100\nP1.1=100000\nB1.1\nS1.1,A1.1,P1.1"
-    // cml = "S1.1=100\nA1.1=100\nP1.1=100000\nB1.1\nS1.1,A1.1,P1.1\nEND"
 
     const port = await navigator.serial.requestPort();
     await port.open({ baudRate: 38400 });
-    const writer = port.writable.getWriter();
-    // for (let cmlChunk of cmlChunkList) {
-    //     if (cmlChunk.length > 0) {
-    //         await writer.write(str2ab(cmlChunk + '\r\n'));
-    //         console.log(cmlChunk + '\r\n')
-    //     }
-    // }
-    // await writer.write(str2ab('END\r\n'));
+    const writer = await port.writable.getWriter();
     await writer.write(str2ab('[1.1' + '\r\n'));
-    console.log('[1.1\r\n');
-    // await writer.write(str2ab('^.1\r\n'));
-    writer.releaseLock();
+    // console.log('[1.1\r\n');
+    await writer.releaseLock();
     await port.close();
 }
 
